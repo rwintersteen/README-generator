2bf1ece22aa89251336fb3e9819783d80dd697f6
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-07-12 14:02:14 -0700

    Initial pushes

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+    // ask questions
+    // assemble Answers
+    // 
+
+// Function call to initialize app
+init();
